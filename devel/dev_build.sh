e5b91793f565e4e91ee0728371a09e75dc66bebb
Michael T. Kloos <michael@michaelkloos.com>
2020-04-25 16:52:15 -0400

    Add tuples to build script

--- devel/dev_build.sh
+++ devel/dev_build.sh
@@ -2,6 +2,8 @@
 
 TEST_FONT_DIR="/usr/share/fonts/dejavu/DejaVuSansMono.ttf"
 
+TUPLE="x86_64-pc-linux-gnu"
+
 CFLAGS="-Wall -std=c99 -pipe -O2 -flto -march=native -g"
 #CFLAGS="$CFLAGS -DDEVEL_STRIP_MCURSOR"
 
@@ -30,47 +32,58 @@ mkdir -p "./build/lib/toolbox"
 mkdir -p "./build/lib/wincomp"
 mkdir -p "./build/lib/wincomp/elements"
 
+# If the variable TUPLE is unset or empty, default to not using a prefix
+CC=""
+LD=""
+if [ -z "$TUPLE" ]; then
+  CC="gcc"
+  LD="ld"
+else
+  CC="${TUPLE}-gcc"
+  LD="${TUPLE}-ld"
+fi
+
 # Complile the core program
 echo "Compile: subatomic.c"
-gcc -c "./../src/core/subatomic.c" -o "./build/core/subatomic.obj" $CFLAGS -DTEST_FONT_DIR="\"$TEST_FONT_DIR\"" &
+$CC -c "./../src/core/subatomic.c" -o "./build/core/subatomic.obj" $CFLAGS -DTEST_FONT_DIR="\"$TEST_FONT_DIR\"" &
 echo "Compile: core/events.c"
-gcc -c "./../src/core/events.c" -o "./build/core/events.obj" $CFLAGS &
+$CC -c "./../src/core/events.c" -o "./build/core/events.obj" $CFLAGS &
 
 # Compile the window elements
 echo "Compile: elements/button.c"
-gcc -c "./../src/lib/wincomp/elements/button.c" -o "./build/lib/wincomp/elements/button.obj" $CFLAGS &
+$CC -c "./../src/lib/wincomp/elements/button.c" -o "./build/lib/wincomp/elements/button.obj" $CFLAGS &
 echo "Compile: elements/checkbox.c"
-gcc -c "./../src/lib/wincomp/elements/checkbox.c" -o "./build/lib/wincomp/elements/checkbox.obj" $CFLAGS &
+$CC -c "./../src/lib/wincomp/elements/checkbox.c" -o "./build/lib/wincomp/elements/checkbox.obj" $CFLAGS &
 echo "Compile: elements/container.c"
-gcc -c "./../src/lib/wincomp/elements/container.c" -o "./build/lib/wincomp/elements/container.obj" $CFLAGS &
+$CC -c "./../src/lib/wincomp/elements/container.c" -o "./build/lib/wincomp/elements/container.obj" $CFLAGS &
 echo "Compile: elements/file_text.c"
-gcc -c "./../src/lib/wincomp/elements/file_text.c" -o "./build/lib/wincomp/elements/file_text.obj" $CFLAGS &
+$CC -c "./../src/lib/wincomp/elements/file_text.c" -o "./build/lib/wincomp/elements/file_text.obj" $CFLAGS &
 echo "Compile: elements/ml_textbox.c"
-gcc -c "./../src/lib/wincomp/elements/ml_textbox.c" -o "./build/lib/wincomp/elements/ml_textbox.obj" $CFLAGS &
+$CC -c "./../src/lib/wincomp/elements/ml_textbox.c" -o "./build/lib/wincomp/elements/ml_textbox.obj" $CFLAGS &
 echo "Compile: elements/radiobutton.c"
-gcc -c "./../src/lib/wincomp/elements/radiobutton.c" -o "./build/lib/wincomp/elements/radiobutton.obj" $CFLAGS &
+$CC -c "./../src/lib/wincomp/elements/radiobutton.c" -o "./build/lib/wincomp/elements/radiobutton.obj" $CFLAGS &
 echo "Compile: elements/tab.c"
-gcc -c "./../src/lib/wincomp/elements/tab.c" -o "./build/lib/wincomp/elements/tab.obj" $CFLAGS &
+$CC -c "./../src/lib/wincomp/elements/tab.c" -o "./build/lib/wincomp/elements/tab.obj" $CFLAGS &
 echo "Compile: elements/textbox.c"
-gcc -c "./../src/lib/wincomp/elements/textbox.c" -o "./build/lib/wincomp/elements/textbox.obj" $CFLAGS &
+$CC -c "./../src/lib/wincomp/elements/textbox.c" -o "./build/lib/wincomp/elements/textbox.obj" $CFLAGS &
 
 # Compile the toolbox
 echo "Compile: cstr_manip.c"
-gcc -c "./../src/lib/toolbox/cstr_manip.c" -o "./build/lib/toolbox/cstr_manip.obj" $CFLAGS &
+$CC -c "./../src/lib/toolbox/cstr_manip.c" -o "./build/lib/toolbox/cstr_manip.obj" $CFLAGS &
 
 # Compile the base library components
 echo "Compile: wincomp.c"
-gcc -c "./../src/lib/wincomp/wincomp.c" -o "./build/lib/wincomp/wincomp.obj" $CFLAGS &
+$CC -c "./../src/lib/wincomp/wincomp.c" -o "./build/lib/wincomp/wincomp.obj" $CFLAGS &
 echo "Compile: wincomp/events.c"
-gcc -c "./../src/lib/wincomp/events.c" -o "./build/lib/wincomp/events.obj" $CFLAGS &
+$CC -c "./../src/lib/wincomp/events.c" -o "./build/lib/wincomp/events.obj" $CFLAGS &
 echo "Compile: wincomp/drawing.c"
-gcc -c "./../src/lib/wincomp/drawing.c" -o "./build/lib/wincomp/drawing.obj" $CFLAGS &
+$CC -c "./../src/lib/wincomp/drawing.c" -o "./build/lib/wincomp/drawing.obj" $CFLAGS &
 echo "Compile: wincomp/text.c"
-gcc -c "./../src/lib/wincomp/text.c" -o "./build/lib/wincomp/text.obj" $CFLAGS &
+$CC -c "./../src/lib/wincomp/text.c" -o "./build/lib/wincomp/text.obj" $CFLAGS &
 echo "Compile: wincomp/elements.c"
-gcc -c "./../src/lib/wincomp/elements.c" -o "./build/lib/wincomp/elements.obj" $CFLAGS &
+$CC -c "./../src/lib/wincomp/elements.c" -o "./build/lib/wincomp/elements.obj" $CFLAGS &
 echo "Compile: wincomp/font.c"
-gcc -c "./../src/lib/wincomp/font.c" -o "./build/lib/wincomp/font.obj" $(pkg-config --cflags freetype2) $CFLAGS &
+$CC -c "./../src/lib/wincomp/font.c" -o "./build/lib/wincomp/font.obj" $(pkg-config --cflags freetype2) $CFLAGS &
 
 # Wait for compilation to finish
 echo "Waiting for compilation to finish."
@@ -79,7 +92,7 @@ echo "Compilation: Complete"
 
 # Link together the element object files
 echo "Link: Element Objects"
-ld $LDFLAGS -r -o "./build/subatomic_p1.obj" \
+$LD $LDFLAGS -r -o "./build/subatomic_p1.obj" \
   "./build/lib/wincomp/elements/button.obj" \
   "./build/lib/wincomp/elements/checkbox.obj" \
   "./build/lib/wincomp/elements/container.obj" \
@@ -90,12 +103,12 @@ ld $LDFLAGS -r -o "./build/subatomic_p1.obj" \
   "./build/lib/wincomp/elements/textbox.obj" &
 
 echo "Link: All Compiled Objects"
-ld $LDFLAGS -r -o "./build/subatomic_p2.obj" \
+$LD $LDFLAGS -r -o "./build/subatomic_p2.obj" \
   "./build/core/subatomic.obj" \
   "./build/core/events.obj" \
   "./build/lib/toolbox/cstr_manip.obj" &
 
-ld $LDFLAGS -r -o "./build/subatomic_p3.obj" \
+$LD $LDFLAGS -r -o "./build/subatomic_p3.obj" \
   "./build/lib/wincomp/wincomp.obj" \
   "./build/lib/wincomp/elements.obj" \
   "./build/lib/wincomp/events.obj" \
@@ -105,7 +118,7 @@ ld $LDFLAGS -r -o "./build/subatomic_p3.obj" \
 
 wait
 echo "Link: Against X11 + FreeType + pThread and Produce Finished Executable ./subatomic.out"
-gcc $CFLAGS $LDFLAGS -o "./subatomic.out" \
+$CC $CFLAGS $LDFLAGS -o "./subatomic.out" \
   "./build/subatomic_p1.obj" \
   "./build/subatomic_p2.obj" \
   "./build/subatomic_p3.obj" \
